220e530dfbdad26f61aaa2b90988082d22ce8def
Luca Musolino <info@lucamusolino.it>
2022-03-28 13:56:10 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### Tailwindcss Typography Shorthand
---
+++ index.js
@@ -0,0 +1,27 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = plugin(function ({ addComponents, e, config }) {
+    const fontSizes = Object.entries(config('theme.fontSize'))
+    const fontWeights = Object.entries(config('theme.fontWeight'))
+    const fontFamilies = Object.entries(config('theme.fontFamily'))
+    const rules = fontSizes.reduce((memo, [sizeKey, sizeValue]) => {
+        fontWeights.forEach(([weightKey, weightValue]) => {
+            const baseStyle = {
+                fontSize: Array.isArray(sizeValue) ? sizeValue[0] : sizeValue,
+                fontWeight: weightValue
+            }
+            if (Array.isArray(sizeValue)) {
+                baseStyle.lineHeight = sizeValue[1]
+            }
+            memo[`.${e(`typo-${sizeKey}-${weightKey}`)}`] = baseStyle
+            fontFamilies.forEach(([familyKey, familyValue]) => {
+                memo[`.${e(`typo-${sizeKey}-${weightKey}-${familyKey}`)}`] = {
+                    ...baseStyle,
+                    fontFamily: Array.isArray(familyValue) ? familyValue.join(', ') : familyValue
+                }
+            })
+        })
+        return memo
+    }, {})
+    addComponents(rules)
+})
